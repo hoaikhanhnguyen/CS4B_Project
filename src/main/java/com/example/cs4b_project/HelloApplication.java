f96b777e58a49fb81db39439204ed6360b92ebbf
resanityyy <36976600+Resetz@users.noreply.github.com>
2024-02-13 15:00:55 -0800

    Update home view stage title to "Tic Tac Toe!"

--- src/main/java/com/example/cs4b_project/HelloApplication.java
+++ src/main/java/com/example/cs4b_project/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Tic Tac Toe!");
         stage.setScene(scene);
         stage.show();
     }
