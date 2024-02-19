292d30627d83b642c5f575613e5342218cea77f5
resanityyy <36976600+Resetz@users.noreply.github.com>
2024-02-18 20:25:15 -0800

    Added Gameboard functionality and Game object.

--- src/main/java/com/example/cs4b_project/Gameboard.java
+++ src/main/java/com/example/cs4b_project/Gameboard.java
@@ -10,19 +10,31 @@ public class Gameboard {
     @FXML
     public Button[] boardArray;
 
+    public Game game;
+    public int player = 1;
+
     public void initialize() {
         boardArray =
             new Button[]{button1, button2, button3,
                          button4, button5, button6,
                          button7, button8, button9};
+        game = new Game();
         for (int i = 0 ; i < 9 ; i++) {
             int buttonPos = i;
             boardArray[i].setOnAction((ActionEvent a) -> {
-                // TODO - Implement proper interaction with a back-end tic-tac-toe board
-                boardArray[buttonPos].setText("X");
+                boardArray[buttonPos].setText(player == 1 ? "X" : "O" );
                 boardArray[buttonPos].setDisable(true);
+
+                // Set Internal Board
+                game.setPos(buttonPos, player);
+
+                // Update Player
+                player = 1+(player%2);
+
+                game.dumpBoard();
                 a.consume();
             });
         }
     }
+
 }
