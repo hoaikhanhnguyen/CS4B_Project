a74ec92c1024d459f9e7d66c9794f4b0e2b05c79
resanityyy <36976600+Resetz@users.noreply.github.com>
2024-02-18 16:11:04 -0800

    Update Gameboard controller to add onAction functionality depending on the button during initialization.

--- src/main/java/com/example/cs4b_project/Gameboard.java
+++ src/main/java/com/example/cs4b_project/Gameboard.java
@@ -1,48 +1,28 @@
 package com.example.cs4b_project;
 
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.fxml.Initializable;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
 public class Gameboard {
 
-    public Button button1,  button2, button3, button4, button5, button6, button7, button8, button9 = null;
+    @FXML
+    public Button button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    @FXML
+    public Button[] boardArray;
 
-    // Need to REFACTOR to make event lister dynamic and reusable
-    public void onCellClick(ActionEvent actionEvent) {
-        //check if which player's turn
-        this.button1.setText("X");
-        //check win condition
-
-        // disable button
-        //this.button1.setDisabled(true);
+    public void initialize() {
+        boardArray =
+            new Button[]{button1, button2, button3,
+                         button4, button5, button6,
+                         button7, button8, button9};
+        for (int i = 0 ; i < 9 ; i++) {
+            int buttonPos = i;
+            boardArray[i].setOnAction((ActionEvent a) -> {
+                // TODO - Implement proper interaction with a back-end tic-tac-toe board
+                boardArray[buttonPos].setText("X");
+                boardArray[buttonPos].setDisable(true);
+                a.consume();
+            });
+        }
     }
-    public void onCellClick2(ActionEvent actionEvent) {
-        this.button2.setText("X");
-    }
-    public void onCellClick3(ActionEvent actionEvent) {
-        this.button3.setText("X");
-    }
-    public void onCellClick4(ActionEvent actionEvent) {
-        this.button4.setText("X");
-    }
-    public void onCellClick5(ActionEvent actionEvent) {
-        this.button5.setText("X");
-    }
-    public void onCellClick6(ActionEvent actionEvent) {
-        this.button6.setText("X");
-    }
-    public void onCellClick7(ActionEvent actionEvent) {
-        this.button7.setText("X");
-    }
-    public void onCellClick8(ActionEvent actionEvent) {
-        this.button8.setText("X");
-    }
-    public void onCellClick9(ActionEvent actionEvent) {
-        this.button9.setText("X");
-    }
-
 }
