996b29ae8d0f9ec8277dff871ac3403bc2e2e626
Khanh Nguyen <con.nguyen.86@gmail.com>
2024-02-20 02:07:34 -0800

    Added Simple Player Turn Indicator

--- src/main/java/com/example/cs4b_project/Gameboard.java
+++ src/main/java/com/example/cs4b_project/Gameboard.java
@@ -8,7 +8,7 @@ public class Gameboard {
 
     @FXML
     public Button button1, button2, button3, button4, button5, button6, button7, button8, button9;
-    public Label turnCount;
+    public Label turnCount, playerOneLabel, playerTwoLabel;
     @FXML
     public Button[] boardArray;
 
@@ -21,6 +21,10 @@ public class Gameboard {
                          button4, button5, button6,
                          button7, button8, button9};
         game = new Game();
+
+        // Set Initial Player Turn Indicator
+        updatePlayerTurnInd(player);
+
         for (int i = 0 ; i < 9 ; i++) {
             int buttonPos = i;
             boardArray[i].setOnAction((ActionEvent a) -> {
@@ -34,13 +38,35 @@ public class Gameboard {
                 player = 1+(player%2);
 
                 // Update turn count
-                int turns = Integer.parseInt(turnCount.getText());
-                turns++;
-                turnCount.setText(Integer.toString(turns));
+                updateTurnCount();
+
+                // Update Player Turn Indicator
+                updatePlayerTurnInd(player);
 
                 game.dumpBoard();
                 a.consume();
             });
+        }
+    }
+
+    public void updateTurnCount() {
+        int turns = Integer.parseInt(turnCount.getText());
+        turns++;
+        turnCount.setText(Integer.toString(turns));
+    }
+
+    public void updatePlayerTurnInd(int player) {
+        if (player == 1) {
+            playerOneLabel.setText("Player 1's Turn!");
+            playerOneLabel.setUnderline(true);
+            playerTwoLabel.setText("Player 2");
+            playerTwoLabel.setUnderline(false);
+        } else {
+            playerOneLabel.setText("Player 1");
+            playerOneLabel.setUnderline(false);
+            playerTwoLabel.setText("Player 2's Turn!");
+            playerTwoLabel.setUnderline(true);
+
         }
     }
 
