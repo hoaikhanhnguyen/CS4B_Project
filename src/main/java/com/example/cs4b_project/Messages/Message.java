c6217fccbd376b4bc3d6cc09f5bb83b193291667
resanityyy <36976600+Resetz@users.noreply.github.com>
2024-05-05 14:49:47 -0700

    (feat) Message object framework

---
+++ src/main/java/com/example/cs4b_project/Messages/Message.java
@@ -0,0 +1,13 @@
+package com.example.cs4b_project.Messages;
+
+public class Message {
+    private final String type;
+
+    public Message(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
